b98279ab168765c02dad93c79a1b2965b70a12bc
gloria <mwongeliglo15@gmail.com>
2020-10-11 01:32:45 +0300

    new commit

---
+++ app/src/main/java/com/gloria/gaming/login_details.java
@@ -0,0 +1,14 @@
+package com.gloria.gaming;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class login_details extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_details);
+    }
+}
