d0984fdbb3e21c0f6f0272d76eaf6018e1629907
gloria <mwongeliglo15@gmail.com>
2020-10-11 09:24:13 +0300

    adding intents

--- app/src/main/java/com/gloria/gaming/Login.java
+++ app/src/main/java/com/gloria/gaming/Login.java
@@ -2,13 +2,37 @@ package com.gloria.gaming;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class Login extends AppCompatActivity {
+    private Button mSignup;
+    private Button mLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        mSignup = (Button) findViewById(R.id.Signup);
+        mSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Login.this, signup_details.class);
+                startActivity(intent);
+                Toast.makeText(Login.this, "please create account", Toast.LENGTH_LONG).show();
+            }
+        });
+        mLogin = (Button) findViewById(R.id.Login);
+        mLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent2 = new Intent(Login.this, login_details.class);
+                startActivity(intent2);
+                Toast.makeText(Login.this, "You can now login", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
--- app/src/main/java/com/gloria/gaming/MainActivity.java
+++ app/src/main/java/com/gloria/gaming/MainActivity.java
@@ -2,13 +2,29 @@ package com.gloria.gaming;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private Button mGo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mGo = (Button)findViewById(R.id.Go);
+        mGo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Login.class);
+                startActivity(intent);
+                Toast.makeText(MainActivity.this, "welcome", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
