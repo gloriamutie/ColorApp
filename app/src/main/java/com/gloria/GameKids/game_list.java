c16b3bd99f613deb66a0a104a24a1a8b80e02807
gloria <mwongeliglo15@gmail.com>
2020-10-18 01:47:38 +0300

    adding youtube api and getting requests

--- app/src/main/java/com/gloria/GameKids/game_list.java
+++ app/src/main/java/com/gloria/GameKids/game_list.java
@@ -5,8 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ListView;
 
+import com.gloria.GameKids.models.Item;
+
+import java.util.List;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class game_list extends AppCompatActivity {
     //private ListView mListView;
@@ -35,6 +42,10 @@ public class game_list extends AppCompatActivity {
        // ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, gamelist);
         MyGamesAdapter adapter = new MyGamesAdapter(this, android.R.layout.simple_list_item_1, gamelist); // must match constructor!
         mListView.setAdapter(adapter);
+
+        YoutubeApi client = YoutubeClient.getClient();
+
+        Call<com.gloria.GameKids.YoutubeGameSearchResponse> call = client.getPlaylists("part","key","channelId");
 
     }
 }
