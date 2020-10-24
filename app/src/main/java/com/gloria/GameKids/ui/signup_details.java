5778c92daa1e36292f7971dc875675ddd3e8af6e
gloria <mwongeliglo15@gmail.com>
2020-10-24 23:51:56 +0300

    adding authentication

--- app/src/main/java/com/gloria/GameKids/ui/signup_details.java
+++ app/src/main/java/com/gloria/GameKids/ui/signup_details.java
@@ -1,37 +1,268 @@
 package com.gloria.GameKids.ui;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
 import com.gloria.GameKids.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class signup_details extends AppCompatActivity {
-    @BindView(R.id.createaccount)
-    Button mCreateaccount;
+public class signup_details extends AppCompatActivity implements View.OnClickListener{
+
+    private static final String TAG = "EmailPassword";
+
+    private Button mGames;
+
+    // [START declare_auth]
+    private FirebaseAuth mAuth;
+    // [END declare_auth]
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_signup_details);
-        ButterKnife.bind(this);
+        mGames = Button.inflate(getLayoutInflater());
+        setContentView(mGames.getRoot());
+        setProgressBar(mGames.progressBar);
 
-        mCreateaccount.setOnClickListener(new View.OnClickListener() {
+        // Buttons
+        mGames.emailSignInButton.setOnClickListener(this);
+        mGames.emailCreateAccountButton.setOnClickListener(this);
+        mGames.signOutButton.setOnClickListener(this);
+        mGames.verifyEmailButton.setOnClickListener(this);
+        mGames.reloadButton.setOnClickListener(this);
+
+        // [START initialize_auth]
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+        // [END initialize_auth]
+    }
+
+    // [START on_start_check_user]
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        updateUI(currentUser);
+    }
+
+
+    private void createAccount(String email, String password) {
+        Log.d(TAG, "createAccount:" + email);
+        if (!validateForm()) {
+            return;
+        }
+
+        showProgressBar();
+
+        // START create_user_with_email
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(signup_details.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
+
+                        // [START_EXCLUDE]
+                        hideProgressBar();
+                        // [END_EXCLUDE]
+                    }
+                });
+        // [END create_user_with_email]
+    }
+
+    private void signIn(String email, String password) {
+        Log.d(TAG, "signIn:" + email);
+        if (!validateForm()) {
+            return;
+        }
+
+        showProgressBar();
+
+        // [START sign_in_with_email]
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            Toast.makeText(signup_details.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            updateUI(null);
+                            // [START_EXCLUDE]
+                            checkForMultiFactorFailure(task.getException());
+                            // [END_EXCLUDE]
+                        }
+
+                        // [START_EXCLUDE]
+                        if (!task.isSuccessful()) {
+                            mGames.status.setText(R.string.auth_failed);
+                        }
+                        hideProgressBar();
+                        // [END_EXCLUDE]
+                    }
+                });
+        // [END sign_in_with_email]
+    }
+
+    private void signOut() {
+        mAuth.signOut();
+        updateUI(null);
+    }
+
+    private void sendEmailVerification() {
+        // Disable button
+        mGames.verifyEmailButton.setEnabled(false);
+
+        // Send verification email
+        // [START send_email_verification]
+        final FirebaseUser user = mAuth.getCurrentUser();
+        user.sendEmailVerification()
+                .addOnCompleteListener(this, new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        // [START_EXCLUDE]
+                        // Re-enable button
+                        mGames.verifyEmailButton.setEnabled(true);
+
+                        if (task.isSuccessful()) {
+                            Toast.makeText(signup_details.this,
+                                    "Verification email sent to " + user.getEmail(),
+                                    Toast.LENGTH_SHORT).show();
+                        } else {
+                            Log.e(TAG, "sendEmailVerification", task.getException());
+                            Toast.makeText(signup_details.this,
+                                    "Failed to send verification email.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                        // [END_EXCLUDE]
+                    }
+                });
+        // [END send_email_verification]
+    }
+
+    private void reload() {
+        mAuth.getCurrentUser().reload().addOnCompleteListener(new OnCompleteListener<Void>() {
             @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(signup_details.this, login_details.class);
-                startActivity(intent);
-                Toast.makeText(signup_details.this, "Account created please login",Toast.LENGTH_LONG).show();
+            public void onComplete(@NonNull Task<Void> task) {
+                if (task.isSuccessful()) {
+                    updateUI(mAuth.getCurrentUser());
+                    Toast.makeText(signup_details.this,
+                            "Reload successful!",
+                            Toast.LENGTH_SHORT).show();
+                } else {
+                    Log.e(TAG, "reload", task.getException());
+                    Toast.makeText(signup_details.this,
+                            "Failed to reload user.",
+                            Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
 
+    private boolean validateForm() {
+        boolean valid = true;
 
+        String email =mGames.fieldEmail.getText().toString();
+        if (TextUtils.isEmpty(email)) {
+            mGames.fieldEmail.setError("Required.");
+            valid = false;
+        } else {
+            mGames.fieldEmail.setError(null);
+        }
+
+        String password = mGames.fieldPassword.getText().toString();
+        if (TextUtils.isEmpty(password)) {
+            mGames.fieldPassword.setError("Required.");
+            valid = false;
+        } else {
+            mGames.fieldPassword.setError(null);
+        }
+
+        return valid;
+    }
+
+    private void updateUI(FirebaseUser user) {
+        hideProgressBar();
+        if (user != null) {
+            mGames.status.setText(getString(R.string.emailpassword_status_fmt,
+                    user.getEmail(), user.isEmailVerified()));
+            mGames.detail.setText(getString(R.string.firebase_status_fmt, user.getUid()));
+
+            mGames.emailPasswordButtons.setVisibility(View.GONE);
+            mGames.emailPasswordFields.setVisibility(View.GONE);
+            mGames.signedInButtons.setVisibility(View.VISIBLE);
+
+            if (user.isEmailVerified()) {
+                mBinding.verifyEmailButton.setVisibility(View.GONE);
+            } else {
+                mBinding.verifyEmailButton.setVisibility(View.VISIBLE);
+            }
+        } else {
+            mGames.status.setText(R.string.signed_out);
+            mGames.detail.setText(null);
+
+            mGames.emailPasswordButtons.setVisibility(View.VISIBLE);
+            mGames.emailPasswordFields.setVisibility(View.VISIBLE);
+            mGames.signedInButtons.setVisibility(View.GONE);
+        }
+    }
+
+//    private void checkForMultiFactorFailure(Exception e) {
+//        // Multi-factor authentication with SMS is currently only available for
+//        // Google Cloud Identity Platform projects. For more information:
+//        // https://cloud.google.com/identity-platform/docs/android/mfa
+//        if (e instanceof FirebaseAuthMultiFactorException) {
+//            Log.w(TAG, "multiFactorFailure", e);
+//            Intent intent = new Intent();
+//            MultiFactorResolver resolver = ((FirebaseAuthMultiFactorException) e).getResolver();
+//            intent.putExtra("EXTRA_MFA_RESOLVER", resolver);
+//            setResult(MultiFactorActivity.RESULT_NEEDS_MFA_SIGN_IN, intent);
+//            finish();
+//        }
+//    }
+
+    @Override
+    public void onClick(View v) {
+        int i = v.getId();
+        if (i == R.id.emailCreateAccountButton) {
+            createAccount(mGames.fieldEmail.getText().toString(), mGames.fieldPassword.getText().toString());
+        } else if (i == R.id.emailSignInButton) {
+            signIn(mGames.fieldEmail.getText().toString(), mGames.fieldPassword.getText().toString());
+        } else if (i == R.id.signOutButton) {
+            signOut();
+        } else if (i == R.id.verifyEmailButton) {
+            sendEmailVerification();
+        } else if (i == R.id.reloadButton) {
+            reload();
+        }
+    }
+}
 }
